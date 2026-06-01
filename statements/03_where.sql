8f67d661b9bf1fbd8cba41759430ad875a77f1a5
Nicolás A <nicolasagudelolopez@gmail.com>
2026-05-31 22:00:18 -0500

    where ejemplos

---
+++ statements/03_where.sql
@@ -0,0 +1,7 @@
+SELECT * FROM users WHERE name = 'Martin';
+
+SELECT name FROM users WHERE birthday LIKE '2008-05-20';
+
+SELECT DISTINCT name FROM users WHERE birthday LIKE '2008-05-20';
+
+SELECT DISTINCT name FROM users WHERE name = 'Martin'
