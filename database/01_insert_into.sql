e2f8eba61ff7f9bff5e02cab2feb404e371e71f1
Nicolás A <nicolasagudelolopez@gmail.com>
2026-06-01 18:23:20 -0500

    insert ejemplos

---
+++ database/01_insert_into.sql
@@ -0,0 +1,4 @@
+-- Se ingresan unicamente los datos puestos al inicio
+INSERT INTO users (user_id, name, surname, age) VALUES (7,'María','López','20')
+
+INSERT INTO users VALUES (null,'María','Hurtado','2000-07-07','2015-07-07',25,'hurtado@gmail.com')
