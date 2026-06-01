4c242c6a4172edfdc51273e7018016403eeac04f
Nicolás A <nicolasagudelolopez@gmail.com>
2026-06-01 18:02:42 -0500

    añadiendo un nuevo comando de null

--- statements/08_null.sql
+++ statements/08_null.sql
@@ -3,3 +3,6 @@ SELECT * FROM users WHERE email IS NULL;
 SELECT * FROM users WHERE email IS NOT NULL;
 
 SELECT * FROM users WHERE email IS NOT NULL AND name = 'Nicolas';
+
+-- Pone un valor por defecto en la consulta si el campo es nulo
+SELECT name, surname, age, IFNULL(email, 'N/A') FROM users;
